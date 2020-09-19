0781464043c3b4582403d5004235a84a7fcfb1d0
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-19 11:58:49 +1000

    41. The Problem with Callbacks: Callback Hell

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
+      if (err) {
+        return console.log(err.message);
+      }
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
